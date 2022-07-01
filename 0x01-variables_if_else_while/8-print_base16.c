658232255a7b1a9d855f59612ddb390b07a05199
ZhDev0 <zhdevz00@gmail.com>
2022-07-01 13:10:46 -0700

    Fixed

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,12 +10,18 @@
  */
 int main(void)
 {
+/**
+ *
+ * hexadecimal
+ * 0 1 2 3 4 5 6 7 8 9 a b c d e f
+ */
+
+char ch;
 int n;
 for (n = 48 ; n <= 58 ; n++)
 {
 putchar(n);
 }
-char ch;
 for (ch = 'a' ; ch <= 'f' ; ch++)
 {
 putchar(ch);
