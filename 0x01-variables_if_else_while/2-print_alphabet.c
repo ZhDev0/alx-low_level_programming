f4815e22b0e560f2654f3f240041a4c694f2aaf2
ZhDev0 <zhdevz00@gmail.com>
2022-07-01 12:27:05 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int ch;
+for (ch = 97 ; ch <= 122 ; ch++)
+{
+putchar(ch);
+}
+putchar("10"); /*this is the ascii code for a new line*/
+return (0);
+}
