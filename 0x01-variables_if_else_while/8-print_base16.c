29a7517646119312b2b7981cec83ab61fdbe2ade
ZhDev0 <zhdevz00@gmail.com>
2022-07-01 13:05:56 -0700

    Task 9

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch;
+int n;
+for (n = 48 ; n <= 58 ; n++)
+{
+putchar(ch);
+}
+for (ch = 'a' ; ch <= 'f' ; ch++)
+{
+putchar(ch);
+}
+putchar(10);
+return (0);
+}
