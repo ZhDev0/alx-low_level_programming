fc4b6665a8a0578ab59967f33504f6c20b01e62b
ZhDev0 <zhdevz00@gmail.com>
2022-07-01 12:52:02 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+for (n = 0 ; n < 10 ; ch++)
+{
+printf("%d\n", n);
+}
+putchar(10); /*this is the ascii code for a new line*/
+return (0);
+}
