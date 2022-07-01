615b8f94c55611f7e8b5effebd7a1384ce7fd9e5
ZhDev0 <zhdevz00@gmail.com>
2022-07-01 12:56:34 -0700

    Task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Description: 'the program's description'
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int ch;
+for (ch = 48 ; ch < 57 ; ch++)
+{
+putchar(ch);
+}
+putchar(10);
+return (0);
+}
