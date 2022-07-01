05c38b2888974207c743f84c2f023a4a00758ed2
ZhDev0 <zhdevz00@gmail.com>
2022-07-01 12:36:04 -0700

    Task 3 Fixed

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,19 +3,19 @@
 #include <time.h>
 /**
  * main - Entry point
- * 
+ *
  * Description: 'the program's description'
- * 
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 int ch;
-for(ch=97 ; ch <= 122 ; ch++)
+for (ch = 97 ; ch <= 122 ; ch++)
 {
 putchar(ch);
 }
-for(ch=65 ; ch <= 90 ; ch++)
+for (ch = 65 ; ch <= 90 ; ch++)
 {
 putchar(ch);
 }
