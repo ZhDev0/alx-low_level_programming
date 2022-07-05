fefe4daed18cafb24f9d2326af7f9a1c1ab87f50
ZhDev0 <zhdevz00@gmail.com>
2022-07-05 09:05:24 -0700

    Fixing Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet -> print the lowercase alphabets
+ */
+
+void print_alphabet_x10(void)
+{
+int x;
+int j;
+for (x = 0 ; x < 10 ; x++)
+{
+for (j = 'a' ; j <= 'z' ; j++)
+{
+_putchar(j);
+}
+}
+_putchar('\n');
+}
