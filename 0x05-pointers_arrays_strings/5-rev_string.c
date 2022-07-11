46721579c21f5da0696c730da7fa04a90fff7ea8
ZhDevO <zairhomar00@gmail.com>
2022-07-11 17:26:07 +0100

    Task 5 Fixed

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,17 +11,17 @@ void rev_string(char *s)
 char temp;
 int i, length1, length2;
 
-lenght1 = 0;
+length1 = 0;
 length2 = 0;
 
 while (s[length1] != '\0')
 	lenght1++;
-lenght2 = length1 -1;
+length2 = length1 -1;
 for (i = 0 ; i < length1 ; i++)
 {
 tmp = s[i];
-s[i] = s[lenght2];
-s[lenght2] = tmp;
+s[i] = s[length2];
+s[length2] = tmp;
 length2 -= 1;
-}
+i}
 }
