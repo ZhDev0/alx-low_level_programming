0587e7169ebe60dd9cc0b81ea4606bd0af6beaa0
ZhDevO <zairhomar00@gmail.com>
2022-07-11 17:42:11 +0100

    Task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * puts2 -> is a function
+ * @str: is a parameter
+ */
+
+
+void puts2(char *str)
+{
+int len, i;
+
+len = strlen(str);
+for (i = 0 ; i < len ; i += 2)
+	_putchar(str[i]);
+-putchar('\n');
+}
