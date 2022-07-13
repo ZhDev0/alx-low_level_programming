b9e823417886fa2b8635dad55c4bcf102904b7d1
ZhDevO <zairhomar00@gmail.com>
2022-07-13 21:54:20 +0100

    Task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * leet -> a leet function
+ * @x: param x
+ * Return: a string
+ */
+
+char *leet(char *)
+{
+int a = 0, b = 0, l = 5;
+char tr[5] = {'A', 'E', '0', 'T', 'L'};
+char trw[5] = {'4', '3', '0', '7', '1'};
+
+while (x[a])
+{
+b = 0;
+
+while(b < l)
+{
+if (x[a] == tr[b] || x[a] -32 == tr[b])
+	x[a] = trw[b];
+b++
+
+}
+a++;
+
+}
+return (x);
+
+
+}
