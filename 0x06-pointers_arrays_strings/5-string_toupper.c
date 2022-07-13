946f8bda0cd6b9e554475ebadb0c34d3fc988161
ZhDevO <zairhomar00@gmail.com>
2022-07-13 21:05:35 +0100

    Task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,13 +2,23 @@
 #include <string.h>
 
 /**
- *
- *
+ * string_toupper -> converting any string to upper case
+ * @x: string parameter
+ * Return: string
  */
 
 char *string_toupper(char *)
 {
+int i = 0;
 
+while (x[i])
+{
+if (x[i] >= 97 && x[i] <= 122)
+	x[i] = x[i] -32;
+i++;
+
+}
+return (x);
 
 
 }
