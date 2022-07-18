93aa4b6928f2b9d01b4ca895fca271e33a6e5ad7
ZhDevO <zairhomar00@gmail.com>
2022-07-18 16:54:27 +0100

    Task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _memset -> this memory set fonction
+ * @s: string
+ * @b: a character
+ * @n: an integer
+ * Return: a string
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+return (memset(s,b,n));
+}
