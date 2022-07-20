f0c2017d447f487d25336125f0775ede2861d6a2
ZhDevO <zairhomar00@gmail.com>
2022-07-20 07:53:09 +0100

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+ * _strlen_recursion -> function to get length of string
+ * @s: string param
+ * Return: length of string
+ */
+
+
+int _strlen_recursion(char *s)
+{
+if (*s == '\0')
+	return;
+s++;
+return (1 + _strlen_revursion(s));
+}
