b00dde0af0b6ff7fcd53bde5de90731443e2af19
ZhDev0 <zhdevz00@gmail.com>
2022-07-25 13:33:20 +0100

    Task 0 Fixed 3rd Time

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,14 +9,14 @@
 
 char *create_array(unsigned int size, char c)
 {
-int i;
+unsigned int i;
 char *s;
 
 if (size == 0)
 {
 return (NULL);
 }
-s = malloc(size * sizeof(char));
+s = (char*)malloc(size * sizeof(char));
 if (s == NULL)
 {
 return (NULL);
